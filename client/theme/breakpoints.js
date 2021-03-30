5b36881ebc768c10218e8943ac7da2ef2a612ac0
Matheus Alves <matheus.alves@meliuz.com.br>
2021-03-29 23:31:37 -0400

    update: adiciona breakpoints ao tema

---
+++ client/theme/breakpoints.js
@@ -0,0 +1,13 @@
+/**
+ * Based in Mobile First always
+ */
+
+ const breakpoints = {
+  xs: '@media (min-width: 0px)',
+  sm: '@media (min-width: 576px)',
+  md: '@media (min-width: 768px)',
+  lg: '@media (min-width: 992px)',
+  xl: '@media (min-width: 1200px)',
+};
+
+export default breakpoints;
--- client/theme/index.js
+++ client/theme/index.js
@@ -1,8 +1,10 @@
+import breakpoints from "./breakpoints";
 import colors from "./colors";
 import space from "./space";
 import typography from "./typography";
 
 export default {
+  breakpoints,
   colors,
   space,
   typography,  
