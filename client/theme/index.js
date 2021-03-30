323f40a8c8f7bcebe16d20041f048c8590f6d95b
Matheus Alves <matheus.alves@meliuz.com.br>
2021-03-29 23:44:00 -0400

    update: adiciona shadow ao tema

--- client/theme/index.js
+++ client/theme/index.js
@@ -1,11 +1,13 @@
 import breakpoints from "./breakpoints";
 import colors from "./colors";
+import shadow from "./shadow";
 import space from "./space";
 import typography from "./typography";
 
 export default {
   breakpoints,
   colors,
+  shadow,
   space,
   typography,  
 };
---
+++ client/theme/shadow.js
@@ -0,0 +1,9 @@
+import { css } from "styled-components";
+
+const base = css`
+  box-shadow: 0 0.125rem rgba(0, 0, 0, 0.4);
+`;
+
+export default {
+  base,
+}
