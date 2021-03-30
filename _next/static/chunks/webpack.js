20af21a503268ff2dc92475f2fa914468203abe4
Matheus Alves <matheus.alves@meliuz.com.br>
2021-03-29 23:58:24 -0400

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5113da30d858b4a02018";
+/******/ 	var hotCurrentHash = "1f777a7ec4a8ed412b65";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ _next/static/y3PLPJoKaugH0SgbLGLJj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-eb89d315d7203f360daa.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ba33a3c5e00bf0537df9.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
