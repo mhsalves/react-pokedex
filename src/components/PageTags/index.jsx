2c3f53e866549da9b4e77c5ac5572678901747c1
Matheus Alves <matheus.alves@meliuz.com.br>
2021-03-30 21:12:52 -0400

    update: adiciona helper static ao icone de PageTags

--- src/components/PageTags/index.jsx
+++ src/components/PageTags/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Head from 'next/head'
 import PropTypes from 'prop-types';
 
+import getStatic from '../../helpers/static';
+
 function PageTags(props) {
   return (
     <Head>
       <title>{props.title}</title>
       <meta name="description" content={props.description} />
-      <link rel="shortcut icon" href="/icon/favicon.png"/>
+      <link rel="shortcut icon" href={getStatic("/icon/favicon.png")}/>
     </Head>
   )
 }
